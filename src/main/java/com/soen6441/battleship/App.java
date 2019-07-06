3e1b04f2db73ce4eb7602f1b8b28913b27aecfa1
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-07-06 00:40:35 -0400

    Dynamic Grid from Start screen

--- src/main/java/com/soen6441/battleship/App.java
+++ src/main/java/com/soen6441/battleship/App.java
@@ -1,6 +1,7 @@
 package com.soen6441.battleship;
 
 
+import com.soen6441.battleship.viewmodels.playerviewmodel.AppWithUI;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -25,46 +26,8 @@ public class App extends Application {
 
 
         primaryStage.setTitle("Battleship");
-        HBox root = new HBox(50);
-        StackPane start = new StackPane();
-        GridPane enemyBoard = new GridPane();
-        GridPane playerBoard = new GridPane();
-
-        for (int rows = 0; rows < 10; rows++) {
-            for (int columns = 0; columns < 10; columns++) {
-                Button enemyCell = new Button();
-                enemyCell.setText(" ");
-                enemyCell.setOnMouseClicked(e -> enemyCell.setStyle("-fx-background-color: #ff0040"));
-                enemyBoard.add(enemyCell, columns, rows);
-            }
-        }
-
-        for (int rows = 0; rows < 10; rows++) {
-            for (int columns = 0; columns < 10; columns++) {
-                Button playerCell = new Button();
-                playerCell.setText(" ");
-                playerCell.setOnMouseClicked(e -> playerCell.setStyle("-fx-background-color: #ff0040"));
-                playerBoard.add(playerCell, columns, rows);
-
-            }
-        }
-        root.getChildren().addAll(playerBoard, enemyBoard);
-
-        // Display start scene
-        Scene scene1 = new Scene(start, 700, 400);
-
-        // Display Game-Play scene
-        Scene scene2 = new Scene(root, 700, 400);
-
-
-        Button startButton = new Button();
-        startButton.setOnAction(e -> primaryStage.setScene(scene2));
-        startButton.setText("Start Game");
-        startButton.setAlignment(Pos.CENTER);
-        start.getChildren().add(startButton);
-
-        primaryStage.setScene(scene1);
-        primaryStage.show();
+        AppWithUI uiObject = new AppWithUI();
+        uiObject.createGame(primaryStage);
 
 
     }
