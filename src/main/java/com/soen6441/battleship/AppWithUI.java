95772c207dc10214b2668d2fa8df3254b621122b
Karandeep Bhardwaj <Karandiip@gmail.com>
2019-07-05 22:32:36 -0400

    Added dynamic grid size property and restrictions to text box

--- src/main/java/com/soen6441/battleship/AppWithUI.java
+++ src/main/java/com/soen6441/battleship/AppWithUI.java
@@ -1,5 +1,6 @@
 package com.soen6441.battleship;
 
+import com.soen6441.battleship.common.Constants;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,18 +13,52 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.sql.SQLOutput;
+
 public class AppWithUI {
+
+    HBox root = new HBox(50);
+    VBox start = new VBox();
+    GridPane enemyBoard = new GridPane();
+    GridPane playerBoard = new GridPane();
+    int gridSize ;
 
     public void createGame(Stage primaryStage) {
 
-        HBox root = new HBox(50);
-        VBox start = new VBox();
+        Button startButton = new Button();
 
-        GridPane enemyBoard = new GridPane();
-        GridPane playerBoard = new GridPane();
+        Scene scene2 = new Scene(root, 700, 400);// starting page of the application
 
-        for (int rows = 0; rows < 10; rows++) {
-            for (int columns = 0; columns < 10; columns++) {
+        startButton.setAlignment(Pos.CENTER);
+        TextField gridSizeTxtField = new TextField();
+        gridSizeTxtField.setAlignment(Pos.BOTTOM_CENTER);
+        startButton.setText("Start Game");
+        start.getChildren().addAll(new Label("Enter the grid size:"), gridSizeTxtField, startButton);
+        gridSizeTxtField.setMaxSize(100,10);
+        start.setAlignment(Pos.CENTER);
+        gridSizeTxtField.textProperty().addListener((obs, oldText, newText) -> {
+            if (newText.matches("^(0|[1-9][0-9]*)$")) {
+                gridSizeTxtField.setText(newText);
+            } else {
+                gridSizeTxtField.setText("");
+            }
+        });
+        startButton.setOnMouseClicked(e ->{
+            gridSize = Integer.parseInt(gridSizeTxtField.getText());
+                createGrids(gridSize);
+                primaryStage.setScene(scene2);
+        });
+
+        //Create scenes of the primary stage
+        Scene scene1 = new Scene(start, 700, 400);
+        primaryStage.setScene(scene1);
+        primaryStage.show();
+    }
+
+    private void createGrids(int gridSize){
+
+        for (int rows = 0; rows < gridSize; rows++) {
+            for (int columns = 0; columns < gridSize; columns++) {
                 Button button = new Button();
                 button.setText(" ");
                 button.setOnMouseClicked(e -> button.setStyle("-fx-background-color: #ff0040"));
@@ -31,30 +66,15 @@ public class AppWithUI {
             }
         }
 
-        for (int rows = 0; rows < 10; rows++) {
-            for (int columns = 0; columns < 10; columns++) {
+        for (int rows = 0; rows < gridSize; rows++) {
+            for (int columns = 0; columns < gridSize; columns++) {
                 Button button1 = new Button();
                 button1.setText(" ");
                 button1.setOnMouseClicked(e -> button1.setStyle("-fx-background-color: #ff0040"));
                 playerBoard.add(button1, columns, rows);
             }
         }
+
         root.getChildren().addAll(playerBoard, enemyBoard);
-        //Create scenes of the primary stage
-        Scene scene1 = new Scene(start, 700, 400);// starting page of the application
-        Button startButton = new Button();
-        Scene scene2 = new Scene(root, 700, 400);// Staging area to place ships
-
-        startButton.setOnAction(e -> primaryStage.setScene(scene2));
-        startButton.setAlignment(Pos.CENTER);
-        TextField gridSize = new TextField();
-        gridSize.setAlignment(Pos.BOTTOM_CENTER);
-        startButton.setText("Start Game");
-        start.getChildren().addAll(new Label("Enter the grid size:"), gridSize, startButton);
-        gridSize.setMaxSize(100,10);
-        start.setAlignment(Pos.CENTER);
-
-        primaryStage.setScene(scene1);
-        primaryStage.show();
     }
 }
--- src/main/java/com/soen6441/battleship/common/Constants.java
+++ src/main/java/com/soen6441/battleship/common/Constants.java
@@ -2,5 +2,5 @@ package com.soen6441.battleship.common;
 
 public class Constants {
 
-    public static final int gridSize = 8;
+    public static int gridSize = 8;
 }
