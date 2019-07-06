0f6d29451babca02f348e9503201abcf72f555a9
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-07-06 19:28:48 -0400

    Enabled start game functionality

--- src/main/java/com/soen6441/battleship/WelcomeScreenController.java
+++ src/main/java/com/soen6441/battleship/WelcomeScreenController.java
@@ -6,27 +6,39 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
+
 
 public class WelcomeScreenController {
     Button button;
     Logger logger = Logger.getLogger(WelcomeScreenController.class.getName());
 
     @FXML
-    protected void start(ActionEvent event) throws Exception {
+    protected void startAction(ActionEvent event) {
         logger.info("Start method was called");
+       try{
+           loadScreen(event);
+       }
+       catch (Exception e){
+            logger.info(e.getMessage());
+       }
+    }
+
+    private void loadScreen(ActionEvent event) throws Exception{
         File file = new File("src/main/resources/GameWindow.fxml");
         Parent root = FXMLLoader.load(file.toURI().toURL());
         Scene gameWindowScene =  new Scene(root,700, 400);
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(gameWindowScene);
         window.show();
+
     }
+
 
 }
