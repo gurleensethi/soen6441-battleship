800652a30f949ce8f6fba2b791247c64eff148f3
Karandeep Bhardwaj <Karandiip@gmail.com>
2019-07-07 17:48:22 -0400

    Added validity to the text field to only take numeric values and alert message for invalid inputs in the field.

--- src/main/java/com/soen6441/battleship/UserInputScreen.java
+++ src/main/java/com/soen6441/battleship/UserInputScreen.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -24,7 +25,19 @@ public class UserInputScreen {
 
     @FXML
     protected void startAction(ActionEvent event) {
-        gridSize = Integer.parseInt(gridSizeField.getText());
+
+        if (gridSizeField.getText() == null || gridSizeField.getText().trim().isEmpty()
+                || !gridSizeField.getText().matches("\\d+")) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning Dialog");
+            alert.setHeaderText("Invalid input type from user!");
+            alert.setContentText("Please input in Numeric from 0-9!");
+            alert.showAndWait();
+            logger.config("Empty or invalid field");
+        }else{
+            gridSize = Integer.parseInt(gridSizeField.getText());
+            logger.info("Value given from user:"+ gridSize);
+        }
         try{
            loadScreen(event);
        }
@@ -34,7 +47,7 @@ public class UserInputScreen {
     }
 
     private void loadScreen(ActionEvent event) throws Exception{
-        File file = new File("src/main/resources/GameWindow.fxml");
+        File file = new File("src/main/resources/WelcomeScreen.fxml");
         Parent root = FXMLLoader.load(file.toURI().toURL());
         Scene userInputScreen =  new Scene(root,700, 400);
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -42,6 +55,4 @@ public class UserInputScreen {
         window.show();
 
     }
-
-
 }
