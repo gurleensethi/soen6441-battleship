a83117e356d8c7edc1b7a26d73e64c6e298098e5
Karandeep Bhardwaj <Karandiip@gmail.com>
2019-07-19 23:19:31 -0400

    Fixed ship placement for one ship

--- src/main/java/com/soen6441/battleship/view/gui/scenes/shipplacement/ShipPlacementGrid.java
+++ src/main/java/com/soen6441/battleship/view/gui/scenes/shipplacement/ShipPlacementGrid.java
@@ -17,7 +17,7 @@ import javafx.scene.layout.GridPane;
 import java.util.*;
 import java.util.logging.Logger;
 
-class ShipPlacementGrid extends GridPane implements EventHandler<ActionEvent> {
+class ShipPlacementGrid extends GridPane implements EventHandler < ActionEvent > {
     private static final Logger logger = Logger.getLogger(ShipPlacementGrid.class.getName());
     private static final String GRID_BUTTON = "GridButton:";
     private static final int MAX_SHIPS = 5;
@@ -29,14 +29,16 @@ class ShipPlacementGrid extends GridPane implements EventHandler<ActionEvent> {
 
     private int numOfShipsPlaced = 0;
     private final int gridSize;
-    private Map<String, Button> buttons = new HashMap<>();
-    private Map<String, Coordinate> buttonCoordinates = new HashMap<>();
-    private Set<String> shipButtonsIds = new HashSet<>();
+    private Map < String,
+            Button > buttons = new HashMap < > ();
+    private Map < String,
+            Coordinate > buttonCoordinates = new HashMap < > ();
+    private Set < String > shipButtonsIds = new HashSet < > ();
     private boolean isSelectingShip = false;
     private Coordinate prevSelectedBtnCoordinate;
-    private PublishSubject<Boolean> isSelectingShipSubject = PublishSubject.create();
-    private PublishSubject<Integer> numShipPlacedSubject = PublishSubject.create();
-    private PublishSubject<Ship> shipAddedPublishSubject = PublishSubject.create();
+    private PublishSubject < Boolean > isSelectingShipSubject = PublishSubject.create();
+    private PublishSubject < Integer > numShipPlacedSubject = PublishSubject.create();
+    private PublishSubject < Ship > shipAddedPublishSubject = PublishSubject.create();
 
     ShipPlacementGrid(int gridSize) {
         this.gridSize = gridSize;
@@ -45,15 +47,15 @@ class ShipPlacementGrid extends GridPane implements EventHandler<ActionEvent> {
         initUI();
     }
 
-    Observable<Boolean> getShipSelectionObservable() {
+    Observable < Boolean > getShipSelectionObservable() {
         return this.isSelectingShipSubject;
     }
 
-    Observable<Integer> getSelectedShipCountObservable() {
+    Observable < Integer > getSelectedShipCountObservable() {
         return this.numShipPlacedSubject;
     }
 
-    Observable<Ship> getShipAddedObservable() {
+    Observable < Ship > getShipAddedObservable() {
         return this.shipAddedPublishSubject;
     }
 
@@ -73,84 +75,68 @@ class ShipPlacementGrid extends GridPane implements EventHandler<ActionEvent> {
 
 
 
-            //Drag over event handler is used for the receiving node to allow movement
+                //Drag over event handler is used for the receiving node to allow movement
                 button.setOnDragOver(event -> {
-
-                   // if(event.getGestureSource() != button && event.getDragboard().hasImage()){
-                        //allow for moving
-                   // System.out.println("drop");
                         event.acceptTransferModes(TransferMode.MOVE);
-
-                    //}
-
-                    event.consume();
-
+                event.consume();
                 });
 
-            //Drag entered changes the appearance of the receiving node to indicate to the player that they can place there
+                //Drag entered changes the appearance of the receiving node to indicate to the player that they can place there
                 button.setOnDragEntered(event -> {
-                    //The drag-and-drop gesture entered the target
-                    //show the user that it is an actual gesture target
-                    if(event.getGestureSource() != button && event.getDragboard().hasImage()){
-                        // source.setVisible(false);
-                        button.setOpacity(0.5);
-                        String str = "";
-                        System.out.println("Drag entered at " + button.getId());
-                    }
-                    event.consume();
+                        //The drag-and-drop gesture entered the target
+                        //show the user that it is an actual gesture target
+                if (event.getGestureSource() != button && event.getDragboard().hasImage()) {
+                    // source.setVisible(false);
+                    button.setOpacity(0.5);
+                    String str = "";
+                    System.out.println("Drag entered at " + button.getId());
+                }
+                event.consume();
 
                 });
 
-
-     //Not working       // Drag dropped draws the image to the receiving node
+                // Drag dropped draws the image to the receiving node
                 button.setOnDragDropped(event -> {
 
-                    //If there is an image on the dragboard, read it and use it
-                    Dragboard db = event.getDragboard();
-                    boolean success = false;
-                    Node node = event.getPickResult().getIntersectedNode();
-                    if(node != button && db.hasImage()){
+                        //If there is an image on the dragboard, read it and use it
+                        Dragboard db = event.getDragboard();
+                boolean success = false;
+                Node node = event.getPickResult().getIntersectedNode();
+                if (node != button && db.hasImage()) {
 
-                        int xShipInitial = buttonCoordinates.get(button.getId()).getX();
-                        int yShipInitial = buttonCoordinates.get(button.getId()).getY();
+                    int xShipInitial = buttonCoordinates.get(button.getId()).getX();
+                    int yShipInitial = buttonCoordinates.get(button.getId()).getY();
 
-                        int xShipFinal = xShipInitial + 1;
-                        int yShipFinal = yShipInitial ;
+                    int xShipFinal = xShipInitial + 1;
+                    int yShipFinal = yShipInitial;
 
-                        ImageView image = new ImageView(db.getImage());
-                        image.setFitWidth(button.getWidth()/2);
-                        image.setFitHeight(button.getHeight()/2);
+                    ImageView image = new ImageView(db.getImage());
+                    image.setFitWidth(button.getWidth() / 2);
+                    image.setFitHeight(button.getHeight() / 2);
 
 
-                        // TODO: set image size; use correct column/row span
-                        System.out.println("Placing ship at " + xShipInitial + ", " + yShipInitial);
+                    // TODO: set image size; use correct column/row span
+                    System.out.println("Placing ship at " + xShipInitial + ", " + yShipInitial);
 
-                        button.setText("");
-                        button.setGraphic(image);
+                    button.setText("");
+                    button.setGraphic(image);
 
-//                         buttons.get(buildButtonId(xShipFinal,yShipFinal)).setText("");
-//                        buttons.get(buildButtonId(xShipFinal,yShipFinal)).setGraphic(image);
+                    //                         buttons.get(buildButtonId(xShipFinal,yShipFinal)).setText("");
+                    //                        buttons.get(buildButtonId(xShipFinal,yShipFinal)).setGraphic(image);
 
-                        success = true;
-                    }
-                    //let the source know whether the image was successfully transferred and used
-                    event.setDropCompleted(success);
+                    success = true;
+                }
+                //let the source know whether the image was successfully transferred and used
+                event.setDropCompleted(success);
 
-                    event.consume();
+                event.consume();
 
                 });
-
-
-
-            //Drag exited reverts the appearance of the receiving node when the mouse is outside of the node
+                //Drag exited reverts the appearance of the receiving node when the mouse is outside of the node
                 button.setOnDragExited(event -> {
-                    //mouse moved away, remove graphical cues
-                   //  source.setVisible(true);
-
-                    button.setOpacity(1);
-
-                    event.consume();
-
+                        //mouse moved away, remove graphical cues
+                        button.setOpacity(1);
+                event.consume();
                 });
 
                 Coordinate coordinate = new Coordinate(x, y);
@@ -209,11 +195,11 @@ class ShipPlacementGrid extends GridPane implements EventHandler<ActionEvent> {
                             // Direction is horizontal
                             shipDirection = ShipDirection.HORIZONTAL;
 
-                            int startX = prevSelectedBtnCoordinate.getX() < coordinate.getX()
-                                    ? prevSelectedBtnCoordinate.getX() : coordinate.getX();
+                            int startX = prevSelectedBtnCoordinate.getX() < coordinate.getX() ?
+                                    prevSelectedBtnCoordinate.getX() : coordinate.getX();
 
-                            int endX = prevSelectedBtnCoordinate.getX() < coordinate.getX()
-                                    ? coordinate.getX() : prevSelectedBtnCoordinate.getX();
+                            int endX = prevSelectedBtnCoordinate.getX() < coordinate.getX() ?
+                                    coordinate.getX() : prevSelectedBtnCoordinate.getX();
 
                             shipLength = endX - startX + 1;
 
@@ -227,11 +213,11 @@ class ShipPlacementGrid extends GridPane implements EventHandler<ActionEvent> {
                             // Direction is vertical
                             shipDirection = ShipDirection.VERTICAL;
 
-                            int startY = prevSelectedBtnCoordinate.getY() < coordinate.getY()
-                                    ? prevSelectedBtnCoordinate.getY() : coordinate.getY();
+                            int startY = prevSelectedBtnCoordinate.getY() < coordinate.getY() ?
+                                    prevSelectedBtnCoordinate.getY() : coordinate.getY();
 
-                            int endY = prevSelectedBtnCoordinate.getY() < coordinate.getY()
-                                    ? coordinate.getY() : prevSelectedBtnCoordinate.getY();
+                            int endY = prevSelectedBtnCoordinate.getY() < coordinate.getY() ?
+                                    coordinate.getY() : prevSelectedBtnCoordinate.getY();
 
                             shipLength = endY - startY + 1;
 
@@ -272,15 +258,15 @@ class ShipPlacementGrid extends GridPane implements EventHandler<ActionEvent> {
 
     private void disableAllButtons() {
         buttons.forEach((buttonId, button) -> {
-            button.setDisable(true);
+                button.setDisable(true);
         });
     }
 
     private boolean isCellValid(Coordinate coordinate) {
-        return coordinate.getX() >= 0
-                && coordinate.getY() >= 0
-                && coordinate.getX() < gridSize
-                && coordinate.getY() < gridSize;
+        return coordinate.getX() >= 0 &&
+                coordinate.getY() >= 0 &&
+                coordinate.getX() < gridSize &&
+                coordinate.getY() < gridSize;
     }
 
     private void enableButtonsOnAxisOfCoordinate(Coordinate coordinate) {
@@ -352,45 +338,45 @@ class ShipPlacementGrid extends GridPane implements EventHandler<ActionEvent> {
             }
         }
 
-//        for (int x = coordinate.getX(); x < gridSize; x++) {
-//            final String buttonId = buildButtonId(x, coordinate.getY());
-//
-//            if (shipButtonsIds.contains(buttonId)) {
-//                break;
-//            }
-//
-//            buttons.get(buttonId).setDisable(false);
-//        }
-//
-//        for (int x = coordinate.getX(); x >= 0; x--) {
-//            final String buttonId = buildButtonId(x, coordinate.getY());
-//
-//            if (shipButtonsIds.contains(buttonId)) {
-//                break;
-//            }
-//
-//            buttons.get(buttonId).setDisable(false);
-//        }
-//
-//        for (int y = coordinate.getY(); y < gridSize; y++) {
-//            final String buttonId = buildButtonId(coordinate.getX(), y);
-//
-//            if (shipButtonsIds.contains(buttonId)) {
-//                break;
-//            }
-//
-//            buttons.get(buttonId).setDisable(false);
-//        }
-//
-//        for (int y = coordinate.getY(); y >= 0; y--) {
-//            final String buttonId = buildButtonId(coordinate.getX(), y);
-//
-//            if (shipButtonsIds.contains(buttonId)) {
-//                break;
-//            }
-//
-//            buttons.get(buttonId).setDisable(false);
-//        }
+        //        for (int x = coordinate.getX(); x < gridSize; x++) {
+        //            final String buttonId = buildButtonId(x, coordinate.getY());
+        //
+        //            if (shipButtonsIds.contains(buttonId)) {
+        //                break;
+        //            }
+        //
+        //            buttons.get(buttonId).setDisable(false);
+        //        }
+        //
+        //        for (int x = coordinate.getX(); x >= 0; x--) {
+        //            final String buttonId = buildButtonId(x, coordinate.getY());
+        //
+        //            if (shipButtonsIds.contains(buttonId)) {
+        //                break;
+        //            }
+        //
+        //            buttons.get(buttonId).setDisable(false);
+        //        }
+        //
+        //        for (int y = coordinate.getY(); y < gridSize; y++) {
+        //            final String buttonId = buildButtonId(coordinate.getX(), y);
+        //
+        //            if (shipButtonsIds.contains(buttonId)) {
+        //                break;
+        //            }
+        //
+        //            buttons.get(buttonId).setDisable(false);
+        //        }
+        //
+        //        for (int y = coordinate.getY(); y >= 0; y--) {
+        //            final String buttonId = buildButtonId(coordinate.getX(), y);
+        //
+        //            if (shipButtonsIds.contains(buttonId)) {
+        //                break;
+        //            }
+        //
+        //            buttons.get(buttonId).setDisable(false);
+        //        }
 
     }
 
