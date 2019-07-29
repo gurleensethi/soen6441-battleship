6abe75080945e5e27739d60a7702e436f0c64523
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-07-29 15:46:20 -0400

    utils test

--- src/test/java/com/soen6441/battleship/utils/RandomShipPlacerTest.java
+++ src/test/java/com/soen6441/battleship/utils/RandomShipPlacerTest.java
@@ -7,14 +7,27 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ *  The type Random ShipPlacer Test.
+ */
+
 public class RandomShipPlacerTest {
     private GameGrid gameGrid;
 
+
+    /**
+     *  Initial setup.
+     */
     @Before
     public void setUp() {
         gameGrid = new GameGrid(8);
     }
 
+
+
+    /**
+     *  Place random ship on the grid.
+     */
     @Test
     public void shipsArePlaceOnBoard() {
         RandomShipPlacer randomShipPlacer = new RandomShipPlacer();
