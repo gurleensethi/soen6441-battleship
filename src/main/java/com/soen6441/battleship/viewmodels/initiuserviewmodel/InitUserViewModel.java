b28ef4972d6feee8c4f60fb85fbed02bbdcc76b0
navi05 <navjyot@navjyots-mbp.wireless.concordia.ca>
2019-08-07 17:46:54 -0400

    Ship scene

--- src/main/java/com/soen6441/battleship/viewmodels/initiuserviewmodel/InitUserViewModel.java
+++ src/main/java/com/soen6441/battleship/viewmodels/initiuserviewmodel/InitUserViewModel.java
@@ -2,6 +2,9 @@ package com.soen6441.battleship.viewmodels.initiuserviewmodel;
 
 import com.soen6441.battleship.services.gameconfig.GameConfig;
 
+/**
+ * The type Init user view model.
+ */
 public class InitUserViewModel implements IInitUserViewModel {
     private GameConfig gameConfig = GameConfig.getsInstance();
 
